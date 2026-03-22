624a6f6f2c116d91e64dd238ec73e9ef94f1abb6
Naodab <nguyenhobadoan110304@gmail.com>
2026-03-22 17:45:01 +0700

    producer upload avatar

---
+++ profileservice/src/main/java/com/naodab/profileservice/dto/event/UpdateAvatarEvent.java
@@ -0,0 +1,19 @@
+package com.naodab.profileservice.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class UpdateAvatarEvent {
+  String email;
+  String avatarUrl;
+}
